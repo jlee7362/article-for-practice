a758fbd1f6c6d9dbae8b46143bc55c5ae14f6303
jlee7362 <jlee736222@gmail.com>
2025-04-18 20:04:10 +0900

    first commit

---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Member {
+    private int id;
+    private String loginiD;
+    private String loginPw;
+    private String name;
+
+    public Member(int id, String loginiD, String loginPw, String name) {
+        this.id = id;
+        this.loginiD = loginiD;
+        this.loginPw = loginPw;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLoginiD() {
+        return loginiD;
+    }
+
+    public String getLoginPw() {
+        return loginPw;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
